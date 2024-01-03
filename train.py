cc074a84c69780677ace9c762ba6ccf71ee0eab7
omics lab @ KAIST <35682945+kaistomics@users.noreply.github.com>
2024-01-03 21:05:21 +0900

    Update train.py

--- train.py
+++ train.py
@@ -1,1 +1,8 @@
 # To generate an input matrix for model training, combine all previously calculated features.
+
+import tensorflow as tf
+from keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from sklearn.model_selection import train_test_split
+import numpy as np
+from numpy import loadtxt
